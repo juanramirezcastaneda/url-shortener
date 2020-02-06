a14a6182b5f70d152b1ae11b1c22621b75c72a63
juanramirezcastaneda <juanramirezcastaneda@gmail.com>
2020-02-06 14:54:09 +0100

    add placeholder methods to vaidate request

--- server.js
+++ server.js
@@ -25,12 +25,28 @@ app.get('/', function (req, res) {
     res.sendFile(process.cwd() + '/views/index.html');
 });
 
+app.post("/api/shorturl/new", function (req, res) {
+    console.log(req);
 
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-    res.json({ greeting: 'hello API' });
+    const urlToShorten = req.body.url;
+    const isUrlValid = validateUrl(urlToShorten);
+    const shortUrl = shortUrl(urlToShorten);
+
+    if (!isUrlValid) {
+        res.json({ error: 'invalid Hostname' })
+    } else {
+        res.json({ 'original_url': urlToShorten, 'short_url': 2 });
+    }
 });
 
 app.listen(port, function () {
     console.log(`Listening to requests on http://localhost:${port}`);
 });
+
+function validateUrl(url) {
+    return false;
+}
+
+function shortUrl(url) {
+    return 2;
+}
